1fb241d6b708938bcbcda8ab066cbf9c50341fb5
Besmir Gjata <besigjata@gmail.com>
2021-04-29 18:01:42 +0200

    added some seeds, route, controller, view for products category

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
 
   validates :title, presence: true
 
-  has_many :product_categories
-  has_many :categories, through: :product_categories
+  has_many :product_categories, dependent: :destroy
+  has_many :categories, through: :product_categories, dependent: :destroy
 end
